80194826d7fc8d27922ce890a5843b9c40248520
Dylan Black <blacktdylan@gmail.com>
2021-09-16 16:25:29 -0500

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from "./styles/logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { withRouter } from "react-router-dom";
+
+function Login({ history }) {
+  return (
+    <>
+      <h1>Facial Emotion Detection Login</h1>
+    </>
+  );
+}
+
+export default withRouter(Login);
