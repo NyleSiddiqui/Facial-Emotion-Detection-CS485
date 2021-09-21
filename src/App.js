3952cce5d9ebc4c007a79eb23e802054812f4ad5
Dylan Black <blacktdylan@gmail.com>
2021-09-21 16:01:32 -0500

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
 import Login from "./pages/Login";
 
