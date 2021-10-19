8233c77530bce689ed7f99c2dd651eaa576e3908
Tyler Bauer <bauerta7262@uwec.edu>
2021-10-19 11:06:31 -0500

    Login - Added success alert for email verification.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,15 +3,22 @@ import { withRouter } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Alert from "react-bootstrap/Alert";
 
 function Login({ history }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [alert, setAlert] = useState(null);
 
   return (
     <div className="login-container">
       <Form className="login">
         <h3>Facial Emotion Detection Log In</h3>
+        {alert && (
+          <Alert variant="success" onClose={() => setAlert(null)} dismissible className="w-auto">
+            Your email has been verified. You can now log in below.
+          </Alert>
+        )}
         <Form.Group className="w-75">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
