0465d7979f7e657a2bb83e02e1dfa359ba150ce2
Tyler Bauer <bauerta7262@uwec.edu>
2021-10-28 16:26:29 -0500

    Added initial rendition of forgot password page.

---
+++ src/pages/ForgotPass.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ForgotPass({ history }) {
+  const [email, setEmail] = useState("");
+
+  return (
+    <div className="login-container">
+      <Form className="login">
+        <h3>Forgot My Password</h3>
+		<p class="w-75">Enter your email address below and you'll be sent a link where you can reset your password.</p>
+        <Form.Group className="w-75">
+			<Form.Label>Email Address</Form.Label>
+			<Form.Control
+				type="email"
+				value={email}
+				onChange={(e) => {
+					setEmail(e.target.value);
+				}}
+				required
+			/>
+        </Form.Group>
+        <div className="d-flex justify-content-between w-75 mt-2">
+          <Button href="/login" variant="outline-dark">
+            <FontAwesomeIcon
+              icon={["fas", "arrow-left"]}
+              style={{ fontSize: "12pt" }}
+            />{" "}Back to Login
+          </Button>
+          <Button type="submit" variant="primary">
+		  	Reset My Password
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export default withRouter(ForgotPass);
