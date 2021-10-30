c1d0562cd7b4919631dd3b705f453b0be7cc0fb6
Mitchell Hanson <hansonmk0604@gmail.com>
2021-10-30 09:52:46 -0500

    Removed compiler error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { Nav } from "react-bootstrap";
-import CopyRightImg from '../images/copyright-img.png';
 import '../styles/App.css';
 
 const Footer = () => {
