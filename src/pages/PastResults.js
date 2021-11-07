d0effedd00cdc589fae4fcc5ebde62fdf83a2fb2
Dylan Black <blacktdylan@gmail.com>
2021-11-07 12:29:19 -0600

    Refractor results page

--- src/pages/PastResults.js
+++ src/pages/PastResults.js
@@ -14,75 +14,71 @@ function PastResults() {
   const { notification, addNotification, removeNotification } =
     useContext(Context);
 
-  const [cards, setCards] = useState("");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     getResults()
       .then((res) => {
-        let columns = [];
-        res.forEach((doc) => {
-          doc = doc.data();
-          console.log(doc);
-          columns.push(
-            <Col>
-              <Card>
-                <Card.Img variant="top" src={doc["img"]} />
-                <Card.Body>
-                  <Card.Title>{doc["emotion"]}</Card.Title>
-                  <Card.Text>Uploaded on {doc["time"]}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          );
-        });
-        setCards(columns);
+        setResults(res);
       })
       .catch((error) => {
         addNotification(error, "danger");
       });
   }, []);
 
-  if (!cards.length) {
-    // If no past results
-    return (
-      <div className="results-container">
-        <div className="results">
-          <p id="verification-paragraph">
-            <b>You do not have images to review!</b>
-          </p>
-          <p id="verification-paragraph">
-            If you would like to see images here, click
-          </p>
-          <p id="verification-paragraph">
-            the link below to go to the upload page!
-          </p>
-          <a id="detect-page-link" href="/detect">
-            Upload page
-          </a>
+  return (
+    <Container>
+      {Object.keys(notification).length !== 0 && (
+        <Alert
+          className="w-100"
+          variant={notification.type}
+          onClose={removeNotification}
+          dismissible
+        >
+          {notification.message}
+        </Alert>
+      )}
+      <h2>Past Results</h2>
+      {results.length !== 0 ? (
+        <>
+          <a href="/">[ Download PDF ]</a>
+          <Row xs={1} sm={3} md={4} lg={5} class="g-4">
+            {results.map((result) => {
+              result = result.data();
+              return (
+                <Col>
+                  <Card>
+                    <Card.Img variant="top" src={result.img} />
+                    <Card.Body>
+                      <Card.Title>{result.emotion}</Card.Title>
+                      <Card.Text>Uploaded on {result.time}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </>
+      ) : (
+        <div className="results-container">
+          <div className="results">
+            <p id="verification-paragraph">
+              <b>You do not have images to review!</b>
+            </p>
+            <p id="verification-paragraph">
+              If you would like to see images here, click
+            </p>
+            <p id="verification-paragraph">
+              the link below to go to the upload page!
+            </p>
+            <a id="detect-page-link" href="/detect">
+              Upload page
+            </a>
+          </div>
         </div>
-      </div>
-    );
-  } else {
-    return (
-      <Container>
-        {Object.keys(notification).length !== 0 && (
-          <Alert
-            className="w-100"
-            variant={notification.type}
-            onClose={removeNotification}
-            dismissible
-          >
-            {notification.message}
-          </Alert>
-        )}
-        <h2>Past Results</h2>
-        <a href="/">[ Download PDF ]</a>
-        <Row xs={1} sm={3} md={4} lg={5} class="g-4">
-          {cards}
-        </Row>
-      </Container>
-    );
-  }
+      )}
+    </Container>
+  );
 }
 
 export default withRouter(PastResults);
