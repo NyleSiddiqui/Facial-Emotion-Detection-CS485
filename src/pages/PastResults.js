9be442a70981b1aca86fc91c788b9a202f19375e
Dylan Black <blacktdylan@gmail.com>
2021-12-01 17:50:54 -0600

    Loading screen on past results page

--- src/pages/PastResults.js
+++ src/pages/PastResults.js
@@ -8,6 +8,7 @@ import Alert from "react-bootstrap/Alert";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import LoadingScreen from "../components/LoadingScreen";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import { PdfDocument } from "../components/ResultsPdf";
 import "../styles/App.css";
@@ -18,6 +19,7 @@ function PastResults() {
     useContext(Context);
 
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     isAuthenticated().then((auth) => {
@@ -25,9 +27,11 @@ function PastResults() {
         getResults()
           .then((res) => {
             setResults(res);
+            setIsLoading(false);
           })
           .catch((error) => {
             addNotification(error, "danger");
+            setIsLoading(false);
           });
       } else {
         history.push("/login");
@@ -37,68 +41,76 @@ function PastResults() {
 
   return (
     <Container>
-      {Object.keys(notification).length !== 0 && (
-        <Alert
-          className="w-100"
-          variant={notification.type}
-          onClose={removeNotification}
-          dismissible
-        >
-          {notification.message}
-        </Alert>
-      )}
-      <h2>Past Results</h2>
-      {results.length !== 0 ? (
-        <>
-          {<PDFDownloadLink
-            document={<PdfDocument data={results} />}
-            fileName="emotion-results.pdf"
-            style={{
-              textDecoration: "none",
-              padding: "10px",
-              color: "#4a4a4a",
-              backgroundColor: "#f2f2f2",
-              border: "1px solid #4a4a4a"
-            }}
-          >
-            {({ blob, url, loading, error }) =>
-              loading ? "Loading document..." : "Download Pdf"
-            }
-          </PDFDownloadLink>}
-          <Row xs={1} sm={3} md={4} lg={5} className="g-4">
-            {results.map((result, idx) => {
-              result = result.data();
-              return (
-                <Col key={idx}>
-                  <Card>
-                    <Card.Img variant="top" src={result.img} />
-                    <Card.Body>
-                      <Card.Title>{result.emotion}</Card.Title>
-                      <Card.Text>Uploaded on {result.time}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              );
-            })}
-          </Row>
-        </>
+      {isLoading ? (
+        <LoadingScreen />
       ) : (
-        <div className="results-container">
-          <div className="results">
-            <p id="verification-paragraph">
-              <b>You do not have images to review!</b>
-            </p>
-            <p id="verification-paragraph">
-              If you would like to see images here, click
-            </p>
-            <p id="verification-paragraph">
-              the link below to go to the upload page!
-            </p>
-            <a id="detect-page-link" href="/detect">
-              Upload page
-            </a>
-          </div>
-        </div>
+        <>
+          {Object.keys(notification).length !== 0 && (
+            <Alert
+              className="w-100"
+              variant={notification.type}
+              onClose={removeNotification}
+              dismissible
+            >
+              {notification.message}
+            </Alert>
+          )}
+          <h2>Past Results</h2>
+          {results.length !== 0 ? (
+            <>
+              {
+                <PDFDownloadLink
+                  document={<PdfDocument data={results} />}
+                  fileName="emotion-results.pdf"
+                  style={{
+                    textDecoration: "none",
+                    padding: "10px",
+                    color: "#4a4a4a",
+                    backgroundColor: "#f2f2f2",
+                    border: "1px solid #4a4a4a",
+                  }}
+                >
+                  {({ blob, url, loading, error }) =>
+                    loading ? "Loading document..." : "Download Pdf"
+                  }
+                </PDFDownloadLink>
+              }
+              <Row xs={1} sm={3} md={4} lg={5} className="g-4">
+                {results.map((result, idx) => {
+                  result = result.data();
+                  return (
+                    <Col key={idx}>
+                      <Card>
+                        <Card.Img variant="top" src={result.img} />
+                        <Card.Body>
+                          <Card.Title>{result.emotion}</Card.Title>
+                          <Card.Text>Uploaded on {result.time}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </>
+          ) : (
+            <div className="results-container">
+              <div className="results">
+                <p id="verification-paragraph">
+                  <b>You do not have images to review!</b>
+                </p>
+                <p id="verification-paragraph">
+                  If you would like to see images here, click
+                </p>
+                <p id="verification-paragraph">
+                  the link below to go to the upload page!
+                </p>
+                <a id="detect-page-link" href="/detect">
+                  Upload page
+                </a>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </Container>
   );
